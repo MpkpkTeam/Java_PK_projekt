f4585281f0345b1e6af0b2c4a5c11f201806e6dc
kordian7 <kordiankurdziel@gmail.com>
2016-06-22 15:34:03 +0200

    asd

--- JavaProject/src/main/java/mpkpk/project/main/Main.java
+++ JavaProject/src/main/java/mpkpk/project/main/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("Test");
+		System.out.println("Test1");
 
 		Hotel hotel = new Hotel("TODO Nazwa");
 	}
